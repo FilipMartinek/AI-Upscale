9da89813620132963e43963099e53793c1f5517e
filip-123 <shrekb@gmail.com>
2023-03-13 18:26:16 +0100

    still some random bugs but some fixed

--- network/model.py
+++ network/model.py
@@ -1,46 +1,50 @@
 # from sklearn import metrics
-import tensorflow as tf
-from tensorflow.keras.layers import Dropout, Flatten, Dense, MaxPooling2D, Conv2D, Input
-from tensorflow.keras.models import Model
-from tensorflow.keras.regularizers import l2
+from tensorflow import keras
+from keras.layers import BatchNormalization, LeakyReLU, Conv2DTranspose, Dropout, Reshape, Input
+from keras.models import Model
+# from tensorflow.keras.regularizers import l2
 
 class MyModel:
 
     def assemble_model(self):
         #input layer
-        input_shape = (64, 64, 2) #can be basically any amount of dimensions
+        input_shape = (64, 64, 3)
         inputs = Input((input_shape))
 
         #hidden layers
         hidden_layers = self.hidden_layers(inputs)
 
         #output layer
-        outputs = self.Convolution(hidden_layers, 128, name="output")
+        x = Conv2DTranspose(1, (5, 5), padding="same", use_bias=False, activation="tanh")(inputs)
+        output = Reshape((64, 64, 3))
 
         #compile
-        model = Model(inputs=[inputs], output=[outputs])
+        model = Model(inputs=[inputs], outputs=[output])
         model.compile(optimizers="Adam", loss=["binary_crossentropy"], metrics={"output":"accuracy"})
 
         return model
     
 
     #Convolution for CNN (convolution neural network)
-    def Convolution(input_tensor, filters, kernel_size=(3, 3), pool_size=(2, 2), strides=(1, 1), name=""):
+    def Convolution(input_tensor, filters, kernel_size=(5, 5), pool_size=(2, 2), strides=(1, 1), name=""):
         
-        x = Conv2D(filters, kernel_size, padding="same", strides=strides, kernel_regularizer=l2(0.001))(input_tensor)
-        x = Dropout(0.1)(x)
+        print(f"---------------input_tensor: {input_tensor}, filters: {filters}")
+        x = Conv2DTranspose(filters, kernel_size, padding="same", strides=strides, use_bias=False)(input_tensor)
+        x = Dropout(0.1)(x) 
+        x = BatchNormalization()(x)
         if name:
-            x = MaxPooling2D(pool_size=pool_size, name=name)(x)
+            x = LeakyReLU(name=name)(x)
         else:
-            x = MaxPooling2D(pool_size=pool_size)(x)
+            x = LeakyReLU()(x)
 
         return x
 
-    def hidden_layers(self, inputs):
+    def hidden_layers(self, input):
         #hidden layers
-        x = self.Convolution(inputs, 64)
-        x = self.Convolution(x, 86)
-        x = self.Convolution(x, 128)
+        print(f"---------------input_tensor: {input}, filters: {64}")
+        x = self.Convolution(input_tensor=input, filters=64)
+        x = self.Convolution(input_tensor=x, filters=86)
+        x = self.Convolution(input_tensor=x, filters=108)
 
         return x
 
